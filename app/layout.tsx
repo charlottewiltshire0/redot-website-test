797faa77480642da60daf21ec3257b5b733bc6f1
Charlotte <charlottewiltshire27@outlook.com>
2024-11-29 18:51:50 +0200

    style: update background color in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${dmSans.className} bg-[#EAEEFE] antialiased`}>
+      <body className={`${dmSans.className} bg-[#FEF1EA] antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
         </ThemeProvider>
