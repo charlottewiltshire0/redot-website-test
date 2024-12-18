69e606ebe91fce28f89becf3de5afc4ccd277cff
Charlotte <charlottewiltshire27@outlook.com>
2024-12-18 18:36:04 +0200

    feat: add sample reviews data

---
+++ constants/reviews.ts
@@ -0,0 +1,57 @@
+interface Reviews {
+  avatar: string;
+  name: string;
+  username: string;
+  body: string;
+}
+
+export const reviews: Reviews[] = [
+  {
+    name: "TimCabbage",
+    username: "@timcabbage",
+    body: "I think it's great.",
+    avatar: "https://image.redotengine.org/avatar/timcabbage.webp",
+  },
+  {
+    name: "Red Otter",
+    username: "@old_school_fool",
+    body: "Everyone on my river uses Redot.",
+    avatar: "https://image.redotengine.org/avatar/old_school_fool.webp",
+  },
+  {
+    name: "Jack the Demon Hunter",
+    username: "@jacking_off_445",
+    body: "I guess it functions.",
+    avatar: "https://image.redotengine.org/avatar/jacking_off_445.webp",
+  },
+  {
+    name: "Cellegen",
+    username: "@cellegenrih",
+    body: "Welp, if it runs my projects faster, then it's great!",
+    avatar: "https://image.redotengine.org/avatar/cellegenrih.webp",
+  },
+  {
+    name: "Takun",
+    username: "@takun",
+    body: "The only thing holding me back in Redot is my coding skills.",
+    avatar: "https://image.redotengine.org/avatar/takun.webp",
+  },
+  {
+    name: "Kabo",
+    username: "@razihara",
+    body: "Cat Crimes >. <",
+    avatar: "https://image.redotengine.org/avatar/razihara.webp",
+  },
+  {
+    name: "mcdubh",
+    username: "@dubhghlasmclaughlin",
+    body: "brb getting another beer.",
+    avatar: "https://image.redotengine.org/avatar/dubhghlasmclaughlin.webp",
+  },
+  {
+    name: "BBQKyle",
+    username: "@BBQKyleGaming",
+    body: "Well put. The @Redot_Engine community is one of the best I've ever been a part of.",
+    avatar: "https://image.redotengine.org/avatar/BBQKyleGaming.webp",
+  },
+];
