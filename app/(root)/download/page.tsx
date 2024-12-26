df93a69b072532b804eec5ad6fd151249dbff94f
Charlotte <charlottewiltshire27@outlook.com>
2024-12-26 13:43:08 +0200

    feat: implement dynamic platform-based download pages

---
+++ app/(root)/download/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { detect } from "detect-browser";
+import { platformMapping } from "@/constants/platformMapping";
+
+export const runtime = "edge";
+
+export default function Download() {
+  const router = useRouter();
+
+  const browser = detect();
+
+  if (!browser) {
+    console.error("Unable to detect browser");
+    return <></>;
+  }
+
+  const rawPlatform =
+    browser.os?.toLowerCase().replace(/\d+/g, "").trim().replace(/\s+/g, "") ||
+    "";
+  const detectedPlatform = platformMapping[rawPlatform] || rawPlatform;
+
+  if (detectedPlatform) {
+    router.push(`/download/${detectedPlatform}`);
+  } else {
+    console.error("Unable to determine platform");
+  }
+
+  return <></>;
+}
