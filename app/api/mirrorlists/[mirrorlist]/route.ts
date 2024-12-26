8b4a46e14cc9de599389f45874ec9bfdd4280483
Charlotte <charlottewiltshire27@outlook.com>
2024-12-26 16:46:07 +0200

    chore: set runtime to edge for mirrorlist API route

--- app/api/mirrorlists/[mirrorlist]/route.ts
+++ app/api/mirrorlists/[mirrorlist]/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-static";
 
+export const runtime = "edge";
+
 export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ mirrorlist: string }> }
