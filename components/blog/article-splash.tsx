5c2efca00d809c65ef742d94fd7c615d55c3895d
Charlotte <charlottewiltshire27@outlook.com>
2025-01-02 01:40:19 +0200

    feat: add ArticleSplash component for blog post preview

---
+++ components/blog/article-splash.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { Tag } from "@/components/blog/tag";
+import { Tag as SchemaTag } from "@/sanity/schemaTypes/tagType";
+import { formatDate } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Post } from "@/sanity/schemaTypes/postType";
+
+export const ArticleSplash = ({ article }: { article: Post }) => {
+  return (
+    <div className="gap flex flex-col gap-5">
+      {article.imageUrl && (
+        <div className="relative aspect-video w-full">
+          <Image
+            src={article.imageUrl}
+            alt={article.title}
+            className="h-full w-full rounded-xl object-cover"
+            fill
+          />
+        </div>
+      )}
+      {article.tags && article.tags.length > 0 && (
+        <div className="flex flex-wrap items-center gap-2">
+          {article.tags?.map((tag: SchemaTag) => (
+            <Tag
+              key={tag.slug.current}
+              className="hover:bg-background"
+              name={tag.name}
+            />
+          ))}
+        </div>
+      )}
+      <h2 className="text-3xl font-bold md:text-4xl">{article.title}</h2>
+      <div className="mb-2 flex flex-row items-center gap-2">
+        <Avatar>
+          <AvatarImage
+            src={article.author?.imageUrl}
+            alt={article.author?.name || "Unknown Author"}
+          />
+          <AvatarFallback>
+            {article.author?.name
+              ? article.author.name.charAt(0).toUpperCase()
+              : ""}
+          </AvatarFallback>
+        </Avatar>
+        <div>
+          <p className="text-sm font-medium">
+            {article.author?.name || "Unknown Author"}
+          </p>
+          <p className="text-xs font-medium text-black/60">
+            {formatDate(article.publishedAt)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
