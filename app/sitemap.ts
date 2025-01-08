7bf8bfae38fa279bb1864148c09616bbff409006
Charlotte <charlottewiltshire27@outlook.com>
2025-01-08 15:56:14 +0200

    feat: update sitemap with new Community page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -29,6 +29,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/community`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/licenses`,
       lastModified: new Date(),
       changeFrequency: "yearly",
