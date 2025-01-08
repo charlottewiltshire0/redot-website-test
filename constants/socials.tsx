1ad3ca3b14727818d2bf735fd00cd21f6bd39839
Charlotte <charlottewiltshire27@outlook.com>
2025-01-08 15:57:58 +0200

    chore: prettier

--- constants/socials.tsx
+++ constants/socials.tsx
@@ -1,3 +1,6 @@
+import { CommunityCardProps } from "@/components/community/CommunityCard";
+import { links } from "@/constants/links";
+
 export interface SocialLink {
   url: string;
   icon: string;
@@ -28,6 +31,8 @@ export const socials: SocialLink[] = [
 
 export const socialsLinks = {
   discord: "https://discord.gg/redot",
+  discordCommunity:
+    "https://discord.com/servers/redot-community-1290063237223551046",
   reddit: "https://www.reddit.com/r/RedotGameEngineMain",
   youtube: "https://www.youtube.com/@ReDotEngine",
   linkedin: "https://www.linkedin.com/company/redot-engine/",
@@ -37,3 +42,84 @@ export const socialsLinks = {
   bsky: "https://bsky.app/profile/redotengine.org",
   itch: "https://redotengine.itch.io/",
 };
+
+export const communityLinks: CommunityCardProps[] = [
+  {
+    imageUrl: "https://image.redotengine.org/socials/reddit.png",
+    label: "Reddit",
+    description: "socialLinks.reddit",
+    href: socialsLinks.reddit,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/youtube.png",
+    label: "YouTube",
+    description: "socialLinks.youtube",
+    href: socialsLinks.youtube,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/x.png",
+    label: "Twitter (X)",
+    description: "socialLinks.x",
+    href: socialsLinks.x,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/xCommunity.png",
+    label: "X Community",
+    description: "socialLinks.xCommunity",
+    href: socialsLinks.xCommunity,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/linkedin.png",
+    label: "LinkedIn",
+    description: "socialLinks.linkedin",
+    href: socialsLinks.linkedin,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/bsky.png",
+    label: "Bluesky",
+    description: "socialLinks.bsky",
+    href: socialsLinks.bsky,
+  },
+];
+
+export const communitySpaces: CommunityCardProps[] = [
+  {
+    imageUrl: "https://image.redotengine.org/socials/discord.png",
+    label: "Discord (Invite)",
+    description: "communitySpacesLinks.discord",
+    href: socialsLinks.discord,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/discordCommunity.png",
+    label: "Discord Community",
+    description: "communitySpacesLinks.discord",
+    href: socialsLinks.discordCommunity,
+  },
+];
+
+export const supportContributions: CommunityCardProps[] = [
+  {
+    imageUrl: "https://image.redotengine.org/socials/ko-fi.png",
+    label: "Ko-fi",
+    description: "contributionsLinks.kofi",
+    href: socialsLinks.kofi,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/itch.png",
+    label: "Itch.io",
+    description: "contributionsLinks.itch",
+    href: socialsLinks.itch,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/github.png",
+    label: "GitHub - Redot Engine",
+    description: "contributionsLinks.github",
+    href: links.github,
+  },
+  {
+    imageUrl: "https://image.redotengine.org/socials/engineExperimental.png",
+    label: "GitHub - Redot Experimental",
+    description: "contributionsLinks.engineExperimental",
+    href: links.engineExperimental,
+  },
+];
