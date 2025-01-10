6bd454732d85316dd709f8b631c4b55c51792088
Charlotte <charlottewiltshire27@outlook.com>
2025-01-10 13:08:17 +0200

    refactor: enhance MobileSidebar UI and update Sheet component behavior

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,15 +1,20 @@
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Link from "next/link";
 import { header } from "@/constants/header";
-import { IconMenu } from "@tabler/icons-react";
+import { IconArrowRight, IconMenu, IconX } from "@tabler/icons-react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import * as SheetPrimitive from "@radix-ui/react-dialog";
+import * as React from "react";
+import { socialsLinks } from "@/constants/socials";
 
 export const MobileSidebar = () => {
   const t = useTranslations("header");
@@ -19,28 +24,57 @@ export const MobileSidebar = () => {
       <SheetTrigger aria-label="Open Menu">
         <IconMenu />
       </SheetTrigger>
-      <SheetContent side="left">
-        <SheetTitle className="mb-4 flex flex-col gap-2 text-xl font-medium">
-          <Image
-            src="/logo.webp"
-            alt="Redot Engine Logo"
-            width={30}
-            height={30}
-            style={{ filter: "invert(100%)" }}
-          />
-          Redot Engine
+      <SheetContent className="max-w-[350px] rounded-r-md pb-6" side="left">
+        <SheetTitle className="mb-4">
+          <div className="flex flex-row items-center justify-between pb-6">
+            <Image
+              src="/logo_large.svg"
+              alt="Redot Engine Logo"
+              width={160}
+              height={50}
+              className="invert"
+              priority
+            />
+            <SheetPrimitive.Close className="rounded-sm opacity-70 ring-offset-background transition-opacity data-[state=open]:bg-secondary hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
+              <IconX className="h-5 w-5" />
+              <span className="sr-only">Close</span>
+            </SheetPrimitive.Close>
+          </div>
+          <Separator />
         </SheetTitle>
-        <div className="flex flex-col items-start justify-start gap-1">
-          {header.map((link) => (
-            <Link key={link.label} href={link.href}>
-              <span className="block text-lg">{t(link.label)}</span>
-            </Link>
-          ))}
-          <Button size="sm" asChild>
-            <Link className="mt-2 w-full" href="/download">
-              {t("downloadButton")}
-            </Link>
-          </Button>
+        <div className="flex h-full flex-col items-start justify-between pb-32">
+          <div className="flex w-full flex-col">
+            {header.map((link) => (
+              <SheetClose key={link.label} asChild>
+                <Link
+                  href={link.href}
+                  className="flex items-center justify-between py-2 text-base font-medium hover:underline sm:px-4"
+                  target={link.newTab ? "_blank" : "_self"}
+                  rel={link.newTab ? "noopener noreferrer" : undefined}
+                >
+                  {t(link.label)}
+                  {link.newTab && <IconArrowRight className="h-4 w-4" />}
+                </Link>
+              </SheetClose>
+            ))}
+          </div>
+
+          <div className="flex w-full flex-col gap-4">
+            <SheetClose asChild>
+              <Button variant="outline" asChild>
+                <Link className="w-full" href={socialsLinks.discord}>
+                  {t("discordButton")}
+                </Link>
+              </Button>
+            </SheetClose>
+            <SheetClose asChild>
+              <Button asChild>
+                <Link className="w-full" href="/download">
+                  {t("downloadButton")}
+                </Link>
+              </Button>
+            </SheetClose>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
