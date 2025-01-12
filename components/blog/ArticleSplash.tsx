5c41cb039af8c4aa8dbe8a66f38d113d71c17066
Charlotte <charlottewiltshire27@outlook.com>
2025-01-12 18:58:34 +0200

    fix: remove fixed aspect ratio from images

--- components/blog/ArticleSplash.tsx
+++ components/blog/ArticleSplash.tsx
@@ -4,48 +4,30 @@ import { Tag as SchemaTag } from "@/sanity/schemaTypes/tagType";
 import { formatDate } from "@/lib/utils";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Post } from "@/sanity/schemaTypes/postType";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { IconArrowLeft } from "@tabler/icons-react";
-import { useTranslations } from "next-intl";
 
 export const ArticleSplash = ({ article }: { article: Post }) => {
-  const t = useTranslations("articleSplash");
-
   return (
-    <div className="flex flex-col gap-5">
-      <div className="space-y-2">
-        <Button variant="link" size="sm" asChild>
-          <Link href="/blog">
-            <IconArrowLeft className="h-5 w-5" />
-            {t("backButton")}
-          </Link>
-        </Button>
-        {article.imageUrl && (
-          <div className="relative aspect-video w-full">
-            <Image
-              src={article.imageUrl}
-              alt={article.title}
-              className="h-full w-full rounded-xl object-cover"
-              fill
-            />
-          </div>
-        )}
-      </div>
-
-      {article.tags && article.tags.length > 0 && (
-        <div className="flex flex-wrap items-center gap-2">
-          {article.tags?.map((tag: SchemaTag) => (
-            <Tag
-              key={tag.slug.current}
-              className="hover:bg-background"
-              name={tag.name}
-            />
-          ))}
+    <div className="flex w-full flex-col items-center gap-8 pb-24">
+      <div className="flex max-w-[768px] flex-col items-center gap-6 text-center md:w-[768px]">
+        <div className="flex flex-col items-center gap-3 text-primary">
+          {article.tags && article.tags.length > 0 && (
+            <div className="flex flex-wrap items-center gap-2">
+              {article.tags?.map((tag: SchemaTag) => (
+                <Tag
+                  key={tag.slug.current}
+                  className="hover:bg-background"
+                  name={tag.name}
+                />
+              ))}
+            </div>
+          )}
+          <h2 className="text-4xl font-bold md:text-6xl">{article.title}</h2>
         </div>
-      )}
-      <h2 className="text-3xl font-bold md:text-4xl">{article.title}</h2>
-      <div className="mb-2 flex flex-row items-center gap-2">
+        <p className="text-lg text-muted-foreground md:text-xl">
+          {article.excerpt}
+        </p>
+      </div>
+      <div className="flex flex-row items-center gap-2">
         <Avatar>
           <AvatarImage
             src={article.author?.imageUrl}
@@ -58,14 +40,24 @@ export const ArticleSplash = ({ article }: { article: Post }) => {
           </AvatarFallback>
         </Avatar>
         <div>
-          <p className="text-sm font-medium">
+          <p className="font-medium">
             {article.author?.name || "Unknown Author"}
           </p>
-          <p className="text-xs font-medium text-black/60">
+          <p className="text-sm font-medium text-muted-foreground">
             {formatDate(article.publishedAt)}
           </p>
         </div>
       </div>
+      {article.imageUrl && (
+        <div className="relative aspect-video w-full lg:w-[1024px]">
+          <Image
+            src={article.imageUrl}
+            alt={article.title}
+            className="h-full w-full rounded-xl object-cover"
+            fill
+          />
+        </div>
+      )}
     </div>
   );
 };
