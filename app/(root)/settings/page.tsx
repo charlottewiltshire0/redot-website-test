927e2dada3eb10736a2ed276e292d8821646a1c4
Charlotte <charlottewiltshire27@outlook.com>
2025-01-16 22:00:25 +0200

    feat(settings): improve settings layout and navigation

--- app/(root)/settings/page.tsx
+++ app/(root)/settings/page.tsx
@@ -1,5 +1,5 @@
 import UnderConstruction from "@/components/UnderConstruction";
 
-export default function Sittings() {
+export default function SettingsAppearancePage() {
   return <UnderConstruction />;
 }
