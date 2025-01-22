ffa2626987c2fa01fe263d29c91d716ea48eb2b9
Charlotte <charlottewiltshire27@outlook.com>
2025-01-22 17:52:18 +0200

    style: improve dark mode compatibility and hover effects

--- components/sections/landing/Review.tsx
+++ components/sections/landing/Review.tsx
@@ -65,8 +65,8 @@ export const Review = () => {
                   />
                 ))}
               </Marquee>
-              <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white"></div>
-              <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white"></div>
+              <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
+              <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
             </div>
           </div>
           <div className="block md:hidden">
