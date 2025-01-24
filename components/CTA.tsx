c7028d31346dea80c29aabe1c2ccb0f11d2e652d
Charlotte <charlottewiltshire27@outlook.com>
2025-01-24 22:36:51 +0200

    refactor: rename SectionWithButtons to CTA and enhance layout

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -28,7 +28,7 @@ interface SectionWithButtonsProps {
   readonly additionalClassNames?: string;
 }
 
-export const SectionWithButtons = ({
+export const CTA = ({
   titleKey,
   descriptionKey,
   buttonLinks,
@@ -55,26 +55,28 @@ export const SectionWithButtons = ({
         transition={{ duration: 0.6 }}
         className="px-5 lg:px-40"
       >
-        <div className="mx-auto max-w-[540px]">
-          <motion.h2
-            initial={{ opacity: 0, y: 20 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5, delay: 0.4 }}
-            className="mt-5 bg-gradient-to-b from-black to-[#7F0000] bg-clip-text text-center text-4xl font-bold tracking-tighter text-transparent dark:from-white dark:to-white md:text-[54px] md:leading-[60px]"
-          >
-            {t(titleKey)}
-          </motion.h2>
+        <div className="mx-auto max-w-[540px] space-y-8">
+          <div className="space-y-5">
+            <motion.h2
+              initial={{ opacity: 0, y: 20 }}
+              animate={inView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.5, delay: 0.4 }}
+              className="bg-gradient-to-b from-black to-[#7F0000] bg-clip-text py-1 text-center text-4xl font-bold tracking-tighter text-transparent dark:from-white dark:to-white md:text-6xl"
+            >
+              {t(titleKey)}
+            </motion.h2>
 
-          <motion.p
-            initial={{ opacity: 0, y: 20 }}
-            animate={inView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5, delay: 0.6 }}
-            className="mt-5 text-center text-xl tracking-tighter text-muted-foreground md:text-[22px] md:leading-[30px]"
-          >
-            {t(descriptionKey)}
-          </motion.p>
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              animate={inView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.5, delay: 0.6 }}
+              className="text-center text-lg tracking-tighter text-muted-foreground md:text-xl"
+            >
+              {t(descriptionKey)}
+            </motion.p>
+          </div>
 
-          <div className="mt-[38px] flex flex-col items-center justify-center gap-1 md:flex-row">
+          <div className="flex flex-col items-center justify-center gap-3 md:flex-row">
             {buttonLinks.map(({ href, labelKey, variant }, index) => (
               <motion.div
                 key={index}
@@ -83,7 +85,12 @@ export const SectionWithButtons = ({
                 transition={{ duration: 0.6, delay: 0.1 * (index + 1) }}
                 className="w-full md:w-auto"
               >
-                <Button className="w-full md:w-auto" variant={variant} asChild>
+                <Button
+                  className="w-full md:w-auto"
+                  size="lg"
+                  variant={variant}
+                  asChild
+                >
                   <Link href={href}>
                     {t(labelKey)}
                     {index === 1 && <IconArrowRight className="h-5 w-5" />}
