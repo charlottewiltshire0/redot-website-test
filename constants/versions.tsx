e7dd094b252e504ebf4d17d4527b9da9eafa3ad4
Charlotte <charlottewiltshire27@outlook.com>
2025-01-29 22:45:00 +0200

    feat: define available download versions with associated icons

---
+++ constants/versions.tsx
@@ -0,0 +1,16 @@
+import { CheckCircle2, Clock, Package } from "lucide-react";
+
+export const versions = [
+  {
+    id: "stable",
+    icon: CheckCircle2,
+  },
+  {
+    id: "latest",
+    icon: Clock,
+  },
+  {
+    id: "mono",
+    icon: Package,
+  },
+];
