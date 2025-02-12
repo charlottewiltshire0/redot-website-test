70cd8213f6f8dcd206036f4e7bea59024077929a
Charlotte <charlottewiltshire27@outlook.com>
2025-02-12 15:13:09 +0200

    chore: adjust Lenis scroll duration for smoother experience

--- components/Lenis.tsx
+++ components/Lenis.tsx
@@ -4,7 +4,7 @@ import ReactLenis from "lenis/react";
 
 const Lenis = ({ children }: PropsWithChildren) => {
   return (
-    <ReactLenis options={{ duration: 2 }} root>
+    <ReactLenis options={{ duration: 1.4 }} root>
       {children}
     </ReactLenis>
   );
