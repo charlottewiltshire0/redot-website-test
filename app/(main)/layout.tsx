4dfc62aeeef91be4b736d6367ab1b826a519545c
Charlotte <charlottewiltshire27@outlook.com>
2025-02-12 15:07:32 +0200

    feat: integrate Lenis for smooth scrolling and refactor layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,6 +3,7 @@
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import CookieConsent from "@/components/CookieConsent";
+import Lenis from "@/components/Lenis";
 
 export default function MainLayout({
   children,
@@ -10,17 +11,19 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <section>
-      <div className="absolute">
-        <CookieConsent
-          onAcceptCallback={() => {}}
-          onDeclineCallback={() => {}}
-        />
-      </div>
+    <Lenis>
+      <section className="scroll-smooth">
+        <div className="absolute">
+          <CookieConsent
+            onAcceptCallback={() => {}}
+            onDeclineCallback={() => {}}
+          />
+        </div>
 
-      <Header />
-      {children}
-      <Footer />
-    </section>
+        <Header />
+        {children}
+        <Footer />
+      </section>
+    </Lenis>
   );
 }
