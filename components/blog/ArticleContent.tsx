4dacd7decf9b0a8ddd3bd45f05445dab1e619412
Charlotte <charlottewiltshire27@outlook.com>
2025-03-03 22:03:58 +0200

    feat: refactor ArticleContent styles for better readability and dark mode compatibility

--- components/blog/ArticleContent.tsx
+++ components/blog/ArticleContent.tsx
@@ -23,7 +23,7 @@ export const ArticleContent = ({ article }: { article: Post }) => {
   return (
     <div>
       <article
-        className="font-in dark:prose-blockquote:border-muted-foreground dark:prose-blockquote:bg-muted-foreground/20 dark:prose-li:marker:text-muted-foreground prose dark:prose-invert sm:prose-base md:prose-lg prose-blockquote:rounded-r-lg prose-blockquote:border-muted prose-blockquote:bg-muted/20 prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:not-italic prose-figure:relative prose-figcaption:mb-2 prose-figcaption:mt-1 prose-li:marker:text-muted-foreground col-span-12 max-w-max first-letter:text-3xl sm:first-letter:text-5xl lg:col-span-8"
+        className="font-in prose col-span-12 max-w-max dark:prose-invert sm:prose-base md:prose-lg first-letter:text-3xl prose-blockquote:rounded-r-lg prose-blockquote:border-muted prose-blockquote:bg-muted/20 prose-blockquote:p-2 prose-blockquote:px-6 prose-blockquote:not-italic prose-figure:relative prose-figcaption:mb-2 prose-figcaption:mt-1 prose-li:marker:text-muted-foreground dark:prose-blockquote:border-muted-foreground dark:prose-blockquote:bg-muted-foreground/20 dark:prose-li:marker:text-muted-foreground sm:first-letter:text-5xl lg:col-span-8"
         dangerouslySetInnerHTML={{ __html: contentHtml }}
       />
     </div>
