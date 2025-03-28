bbc052192eb20f1465a879d32bda7f8775938f31
Charlotte <142303077+charlottewiltshire0@users.noreply.github.com>
2025-03-28 13:01:56 +0200

    Update page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useTranslations } from "next-intl";
 
 const Features = dynamic(
   () =>
@@ -36,8 +35,6 @@ const Start = dynamic(
 );
 
 export default function Landing() {
-  const t = useTranslations("textRevealSection");
-
   return (
     <main>
       <Features />
