bdef7d28fe359ba7323a5fb9efcc72fd73b5dbf0
Charlotte <142303077+charlottewiltshire0@users.noreply.github.com>
2025-03-28 12:39:45 +0200

    Update page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,35 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Hero } from "@/components/sections/landing/Hero";
-import { ProductShowcase } from "@/components/sections/landing/ProductShowcase";
-import { TextReveal } from "@/components/ui/text-reveal";
 import { useTranslations } from "next-intl";
-
-const Features = dynamic(
-  () =>
-    import("@/components/sections/landing/Features").then(
-      (mod) => mod.Features
-    ),
-  {
-    ssr: false,
-  }
-);
-
-const Review = dynamic(
-  () =>
-    import("@/components/sections/landing/Review").then((mod) => mod.Review),
-  {
-    ssr: false,
-  }
-);
-
-const Blog = dynamic(
-  () => import("@/components/sections/landing/Blog").then((mod) => mod.Blog),
-  {
-    ssr: false,
-  }
-);
 
 const Start = dynamic(
   () => import("@/components/sections/landing/Start").then((mod) => mod.Start),
@@ -43,12 +15,6 @@ export default function Landing() {
 
   return (
     <main>
-      <Hero />
-      <ProductShowcase />
-      <TextReveal text={t("body")} />
-      <Features />
-      <Blog />
-      <Review />
       <Start />
     </main>
   );
