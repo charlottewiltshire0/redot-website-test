0a6b403d0477dba9b5ba600f7009cb0385301370
Charlotte <142303077+charlottewiltshire0@users.noreply.github.com>
2025-03-28 13:17:57 +0200

    Update page.tsx

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,18 +1,9 @@
 "use client";
 
-import dynamic from "next/dynamic";
-
-const Start = dynamic(
-  () => import("@/components/sections/landing/Start").then((mod) => mod.Start),
-  {
-    ssr: false,
-  }
-);
-
 export default function Landing() {
   return (
     <main>
-      <Start />
+      test
     </main>
   );
 }
